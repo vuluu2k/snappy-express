a2a814f58280c5e67ea8e04a645b61c9b2181107
vuluu <vuluu040320@gmail.com>
2022-07-29 11:47:14 +0700

    update

---
+++ src/components/SnyStatus.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class SnyStatus extends Component {
+  render() {
+    const { status, statusArray, status_vi, type, label, children, tooltip, badge, style, className } = this.props;
+
+    const checkStatus = statusArray.find(item => item.array.includes(status));
+    return (
+      <div className={`sny-status ${status && 'w-148'} ${type} ${checkStatus && checkStatus?.color} ${className}`} style={style}>
+        {label || children || status_vi}
+      </div>
+    );
+  }
+}
+
+SnyStatus.propTypes = {
+  statusArray: PropTypes.array,
+  status: PropTypes.string,
+  status_vi: PropTypes.string,
+  type: PropTypes.string,
+  label: PropTypes.string,
+  children: PropTypes.node,
+  tooltip: PropTypes.object,
+  badge: PropTypes.object,
+  style: PropTypes.object,
+  className: PropTypes.string,
+};
+
+SnyStatus.defaultProps = {
+  statusArray: [
+    { array: ['request_received', 'waiting_for_return', 'returning', 'part_delivery'], color: 'orange' },
+    { array: ['processing_picked_up', 'out_for_delivery', 'picked_up'], color: 'blue' },
+    { array: ['import_picking_warehouse', 'on_the_way', 'import_returning_warehouse', 'returned'], color: 'cyan' },
+    { array: ['picked_up_fail', 'undeliverable', 'return_fail', 'canceled'], color: 'red' },
+    { array: ['processing_on_the_way', 'on_the_way_returning'], color: 'purple' },
+    { array: ['delivered'], color: 'green' },
+  ],
+  status: undefined,
+  status_vi: 'SnappyExpress',
+  type: '',
+  label: undefined,
+  children: undefined,
+  tooltip: {},
+  badge: {},
+  style: {},
+  className: '',
+};
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import SnyStatus from "./components/SnyStatus";
+
+const returnLibrary = () => {
+  return {
+    SnyStatus: SnyStatus
+      // you can add here other components that you want to export
+  }
+}
+export default returnLibrary()
